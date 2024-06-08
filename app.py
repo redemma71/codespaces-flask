241cd978e9d391955c54d49bb30f5e3497db7f7b
Chad David Cover <hackdcover@gmail.com>
2024-06-08 02:22:46 +0000

    Added yadda.html.

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+@app.route("/yadda")
+def yadda():
+    return render_template("yadda.html",title="Yadda Yadda Yadda")
